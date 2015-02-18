1cd47711557dc9a8bd8f03b4e3f44e46bdb1437a
SimonRAllis <simon.allis@digital.landregistry.gov.uk>
2015-02-18 14:33:25 +0000

    resolve merge

--- application/server.py
+++ application/server.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from application import app
 from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html')
+
+    return render_template('index.html')
