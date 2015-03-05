4c6221193a233f074f8b753a51677ec58c11b6ff
alex blewett <alex.blewett@digital.landregistry.gov.uk>
2015-03-05 12:52:00 +0000

    heroku test4

--- application/server.py
+++ application/server.py
@@ -2,14 +2,13 @@ from flask import render_template, flash, redirect
 from application import app
 from flask import render_template
 from forms import searchForm
-#from ElastSearch import SearchDataOnMeta
+from ElastSearch import SearchDataOnMeta
 
 @app.route('/')
 @app.route('/index', methods=['GET','POST'])
 def index():
-    #form = searchForm()
-    #return render_template('index.html', form=form)
-    return 'hello word'
+    form = searchForm()
+    return render_template('index.html', form=form)
 
 @app.route('/search', methods=['GET','POST'])
 def searchResult():
@@ -21,16 +20,15 @@ def searchResult():
     SubtitleList = []
     
     #flash('Searched for "%s"' %(form.searchString.data))
-    #res = SearchDataOnMeta(form.searchString.data)
+    res = SearchDataOnMeta(form.searchString.data)
     
     #print res
-    #for hit in res['hits']['hits']:
-     #   BodyList.append(hit["_source"]["body"])
-    #    TitleList.append(hit["_source"]["title"])
-    #    SubtitleList.append(hit["_source"]["sub title"])
+    for hit in res['hits']['hits']:
+        BodyList.append(hit["_source"]["body"])
+        TitleList.append(hit["_source"]["title"])
+        SubtitleList.append(hit["_source"]["sub title"])
     #print SearchList[0]
-    #return render_template('searchResult.html',titleElements=TitleList, bodyElements=BodyList, subtitleElements=BodyList)
-    return render_template('searchResult.html')
+    return render_template('searchResult.html',titleElements=TitleList, bodyElements=BodyList, subtitleElements=BodyList)
    
     
     
