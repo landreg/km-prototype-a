0ba7b497b4a12929c2e00d0cfa33f7662334b190
SimonRAllis <simon.allis@digital.landregistry.gov.uk>
2015-03-06 15:14:13 +0000

    Standard style

--- application/server.py
+++ application/server.py
@@ -4,40 +4,35 @@ from flask import render_template
 from forms import searchForm
 from ElastSearch import SearchDataOnMeta, SearchDataOnId
 
+#Store current item ID - defualt to first item
 storeditemid = 1
 
-
+#Demonstration landing page
 @app.route('/')
+@app.route('/index')
 def landing():
     return render_template('landing.html')
 
+#Static demostration pages
+@app.route('/lr-content-static')
+def lrcontentstatic():
+    return render_template('lr-content.html')
 
-@app.route('/test', methods=['GET','POST'])
-def displaySearchResult():
+@app.route('/gov-content-static')
+def govcontentstatic():
+    return render_template('gov-content.html')
 
-    b = '<h2>Bankruptcy - Applications for Form J restrictions</h2>' + '<p class="lead">' + 'Where one or more of joint registered proprietors is subject to a petition in bankruptcy or bankruptcy order, neither a bankruptcy notice nor a bankruptcy restriction will be entered. However, the Official Receiver or trustee in bankruptcy may apply in form RX1 for a Form J restriction once the bankruptcy order has been made.</p>'
+@app.route('/lr-content-std-static')
+def lrcontentstdstatic():
+    return render_template('lr-content-std.html')
 
-
-    page = b
-    return render_template('test.html',searchElements=page)
-
-@app.route('/gov-base', methods=['GET','POST'])
-def footer():
-    form = searchForm()
-    return render_template('gov-base.html')
-
-#@app.route('/test', methods=['GET','POST'])
-#def test():
-#    form = searchForm()
-#    return render_template('test.html')
-
-
-@app.route('/index', methods=['GET','POST'])
+#Test search of elasticsearch
+@app.route('/search', methods=['GET'])
 def index():
     form = searchForm()
     return render_template('index.html', form=form)
 
-@app.route('/search', methods=['GET','POST'])
+@app.route('/search-result', methods=['POST'])
 def searchResult():
     form = searchForm()
 
@@ -46,45 +41,17 @@ def searchResult():
     TitleList = []
     SubtitleList = []
 
-    #flash('Searched for "%s"' %(form.searchString.data))
     res = SearchDataOnMeta(form.searchString.data)
 
-    #print res
     for hit in res['hits']['hits']:
         BodyList.append(hit["_source"]["body"])
         TitleList.append(hit["_source"]["title"])
         SubtitleList.append(hit["_source"]["sub title"])
-    #print SearchList[0]
+
     return render_template('searchResult.html',titleElements=TitleList, bodyElements=BodyList, subtitleElements=BodyList)
 
 
-
-@app.route('/page/<int:itemid>', methods=['GET'])
-def displayPage(itemid):
-
-    global storeditemid
-
-    storeditemid = itemid
-    res = SearchDataOnId(str(itemid))
-
-    for hit in res['hits']['hits']:
-        body = (hit["_source"]["body"])
-        title = (hit["_source"]["title"])
-        subtitle = (hit["_source"]["sub title"])
-
-    print storeditemid
-
-    return render_template('page.html',searchElements=body)
-
-@app.route('/lr-content-static', methods=['GET'])
-def lrcontentstatic():
-    form = searchForm()
-    return render_template('lr-content.html')
-
-@app.route('/gov-content-static')
-def govcontentstatic():
-    return render_template('gov-content.html')
-
+#Select your theme
 @app.route('/lr-page/<int:itemid>', methods=['GET'])
 def displayLrPage(itemid):
 
@@ -98,41 +65,7 @@ def displayLrPage(itemid):
         title = (hit["_source"]["title"])
         subtitle = (hit["_source"]["sub title"])
 
-    print storeditemid
-
     return render_template('lr-page.html',searchElements=body)
-
-@app.route('/lr-page', methods=['GET'])
-def lrcontent():
-        global storeditemid
-
-        itemid = storeditemid
-        res = SearchDataOnId(str(itemid))
-
-        for hit in res['hits']['hits']:
-            body = (hit["_source"]["body"])
-            title = (hit["_source"]["title"])
-            subtitle = (hit["_source"]["sub title"])
-
-        print storeditemid
-
-        return render_template('lr-page.html',searchElements=body)
-
-@app.route('/gov-page', methods=['GET'])
-def govcontent():
-        global storeditemid
-
-        itemid = storeditemid
-        res = SearchDataOnId(str(itemid))
-
-        for hit in res['hits']['hits']:
-            body = (hit["_source"]["body"])
-            title = (hit["_source"]["title"])
-            subtitle = (hit["_source"]["sub title"])
-
-        print storeditemid
-
-        return render_template('page.html',searchElements=body)
 
 @app.route('/gov-page/<int:itemid>', methods=['GET'])
 def displayGovPage(itemid):
@@ -147,6 +80,19 @@ def displayGovPage(itemid):
         title = (hit["_source"]["title"])
         subtitle = (hit["_source"]["sub title"])
 
-    print storeditemid
-
     return render_template('page.html',searchElements=body)
+
+@app.route('/lr-page-std/<int:itemid>', methods=['GET'])
+def displayLrPageStd(itemid):
+
+    global storeditemid
+
+    storeditemid = itemid
+    res = SearchDataOnId(str(itemid))
+
+    for hit in res['hits']['hits']:
+        body = (hit["_source"]["body"])
+        title = (hit["_source"]["title"])
+        subtitle = (hit["_source"]["sub title"])
+
+    return render_template('lr-page-std.html',searchElements=body)
